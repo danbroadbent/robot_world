85879bd79608cfe6b7774b2e2c8a08e787750afd
Daniel Broadbent <dbroadbent85@gmail.com>
2016-08-16 15:42:06 -0600

    added app folder

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,6 +4,9 @@ class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :method_override, true
 
-
+  def robot_manager
+      database = YAML::Store.new('db/taskmanager')
+      @robot_manager ||= RobotManager.new(database)
+    end
 
 end
